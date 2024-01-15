da9b9874db3a7f9493900e63453d0f1b135cc865
fkingsh <12200403@inha.edu>
2024-01-15 10:44:57 +0900

    v0.3 python day02

---
+++ hello.py
@@ -0,0 +1,11 @@
+# print("Hello Python~")
+univ = "Inha university"
+print(univ)
+print(univ[5])
+# univ[5] = 'U' // immutable
+# print(univ)
+subjects = ['python', 'c++', 'linux' , 'data structure' , 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structure & algorithm'  # mutable
+print(subjects)
