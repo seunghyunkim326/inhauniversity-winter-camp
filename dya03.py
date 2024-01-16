88b219e1706c33c6e3e19cc8992237a486294de4
fkingsh <12200403@inha.edu>
2024-01-16 11:39:41 +0900

    v07

--- dya03.py
+++ dya03.py
@@ -1,18 +1,6 @@
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-# print(course)
-# course = course.replace('KEB' , 'Inha' , 2)     #갯수도 설정 가능
-# print(course)
-# print(course.replace('KEB' , 'Inha'))   #.replace 값은 literal 값이다. 메모리값을 바꾸지 않는다.
-# print(course)
-# course = course.replace('KEB' , 'Inha') #메모리값 바꾸려면 재할당 필요
-# print(course)
-# print(course.strip())   #기본은 공란을 자름
-# print(course.strip('!#.'))
-# print(course.strip('.#!*'))     #연속된 것만 자름
-
-print(course.find('KEB'))
-print(course.rfind('KEB'))
-print(course.index('KEB'))
-print(course.rindex('KEB'))
-print(course.find('Inha'))  # -1
-print(course.index('Inha')) # ValueError : substring not found
+subjects = "python c++ database linux"
+subject = input("수강긴청과목 입력 : ")
+if subjects.find(subject) != -1 :
+    print(f'해당 과목은 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.')
+else :
+    print('해당과목이 존재하지 않습니다.')
