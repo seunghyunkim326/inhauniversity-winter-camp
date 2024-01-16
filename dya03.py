a219afebcb77e418e0af0842032cb101e12041cf
fkingsh <12200403@inha.edu>
2024-01-16 14:37:08 +0900

    v11 prime number

--- dya03.py
+++ dya03.py
@@ -1,5 +1,5 @@
-subjects = {'python' : 'kim', 'c++' : 'sung', 'data structure' : 'kim', 'database' : 'kang'}
-print("{0[python]} {0[data structure]}".format(subjects))
+# subjects = {'python' : 'kim', 'c++' : 'sung', 'data structure' : 'kim', 'database' : 'kang'}
+# print("{0[python]} {0[data structure]}".format(subjects))
 # break 가장 가까운 반복문 탈출
 # continue 아래 내용 무시하고 가까운 반복문 다시 돌림
 # 소수 판별기 만들어보기
@@ -10,13 +10,16 @@ print("{0[python]} {0[data structure]}".format(subjects))
 
 
 number = int(input("Input number : "))
-cnt = 0
+is_prime = True # int -> bool
 i = 2
 while i < number:
     if number % i == 0 :
-        cnt += 1
+        is_prime = False    # remove +
+        break       # break를 통해서 시간, 불필요 계산을 줄일 수 있음
+        #print(i, end=' ')
     i += 1
-if cnt == 0:
+# if cnt == 0 :
+if is_prime:    # remove ==
     print(f'{number} is prime number')
 else :
     print(f'{number} is NOT prime number!')
