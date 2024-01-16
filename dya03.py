5344c80b0d6e2add61bfd49760ba3ed371762753
fkingsh <12200403@inha.edu>
2024-01-16 14:06:13 +0900

    10 prime number

--- dya03.py
+++ dya03.py
@@ -11,12 +11,12 @@ print("{0[python]} {0[data structure]}".format(subjects))
 
 number = int(input("Input number : "))
 cnt = 0
-i = 1
-while i <= number:
+i = 2
+while i < number:
     if number % i == 0 :
         cnt += 1
     i += 1
-if cnt == 2:
+if cnt == 0:
     print(f'{number} is prime number')
 else :
     print(f'{number} is NOT prime number!')
