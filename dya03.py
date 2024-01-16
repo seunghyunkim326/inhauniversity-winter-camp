f66138089ced14c4b8a195ac1eac1e21d6dde8dd
fkingsh <12200403@inha.edu>
2024-01-16 11:19:52 +0900

    v04

--- dya03.py
+++ dya03.py
@@ -1,15 +1,9 @@
-# numbers = input("FirstNumber SecondNumber : ").split()
-# print(int(numbers[0]) + int(numbers[1]))    #arithmetic operation
-# print(numbers[0] + numbers[1])    #concatenation
+course = "KEB 2024 KEB a Bootcamp KEB"
+print(course)
+course = course.replace('KEB' , 'Inha' , 2)     #갯수도 설정 가능
+print(course)
+print(course.replace('KEB' , 'Inha'))   #.replace 값은 literal 값이다. 메모리값을 바꾸지 않는다.
+print(course)
+course = course.replace('KEB' , 'Inha') #메모리값 바꾸려면 재할당 필요
+print(course)
 
-# course = "2024 KEB Bootcamp"
-# print(course)
-# # list_course = course.split()    #split(input standard for split) default value => ' '   문자열을 list로
-# list_course = course.split('B')
-# print(list_course)
-
-# join() => list를 문자열로
-
-subjects = ["python" , "c++" , "database"]
-subjects_string = "".join(subjects)
-print(subjects_string)
