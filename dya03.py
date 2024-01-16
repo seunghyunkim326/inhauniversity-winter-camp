56d9fabe83d0c211e0e15d89884458593cb03722
fkingsh <12200403@inha.edu>
2024-01-16 16:33:59 +0900

    ìí화 final

--- dya03.py
+++ dya03.py
@@ -2,9 +2,12 @@ numbers = input("Input first second number : ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
 
+
+if n1 > n2 :
+    n1, n2 = n2, n1
+
 for number in range(n1 , n2+1):
     is_prime = True  # int -> bool
-    i = 2
     if number < 2:
         pass
     else:
@@ -14,3 +17,4 @@ for number in range(n1 , n2+1):
                 break
         if is_prime :
             print(number, end=' ')
+# 어제 과제 화씨 섭씨 4번 메뉴 구간 소수판정 3번 메뉴 소수판정 5번 메뉴 탈출 + 연습문제 p.143 6.5 연습문제
