458bf71fe25d953042e8d91ffd36456c7305a810
fkingsh <12200403@inha.edu>
2024-01-16 15:27:53 +0900

    심화

--- dya03.py
+++ dya03.py
@@ -1,19 +1,16 @@
-number = int(input("Input number : "))
-is_prime = True  # int -> bool
-i = 2
-if number < 2:
-    print(f'{number} is NOT prime number!')
-else:
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-    # if cnt == 0 :
-    if is_prime:    # remove ==
-            print(f'{number} is prime number')
-    else :
-            print(f'{number} is NOT prime number!')
-# assignment : 더 보완할 것을 찾는 것
-# sequence type : 순서를 가지는 값들        for in 구문에서 sequence type 사용가능
-# else 가 for 문과 짝이 될 수 있다!!
-# range()는 generator는 메모리에 저장 X     for문 감소형식 및 계단형식으로 사용 가능
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+for number in range(n1 , n2+1):
+    is_prime = True  # int -> bool
+    i = 2
+    if number < 2:
+        pass
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime :
+            print(number, end=' ')
