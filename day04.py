6203861897027f812187703bbf5f1e654b64d3e9
fkingsh <12200403@inha.edu>
2024-01-17 18:44:22 +0900

    day4 assignment3 8.6 2)

--- day04.py
+++ day04.py
@@ -1,3 +1,4 @@
 # e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
 e2f = dict(dog='chien', cat='chat', walrus='morse')
-print(e2f)
+print(e2f.get('walrus'))        # 8.6 2)
+print(e2f.keys())
