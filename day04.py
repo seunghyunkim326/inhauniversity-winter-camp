d692f33a5c5119b41ddcfb9cba21761aeac2612b
fkingsh <12200403@inha.edu>
2024-01-17 17:58:32 +0900

    v4.5

--- day04.py
+++ day04.py
@@ -1,13 +1,13 @@
-# squares = []
-# for i in range(1,6):
-#     squares.append(i*i)
-# print(squares)
-
-# list comprehension
-# squares = [i*i for i in range(1,6)]
-# print(squares)
-# numbers = [i for i in range(1,6) if i % 2 == 1]
-# print(numbers)
-
-even_squares = [i*i for i in range(1,6) if i % 2 == 0]
-print(even_squares)
+sugang = dict(python="kim", cpp="sung", db="kang")
+# print(sugang)
+sugang['datastructure'] = 'kim'     #add
+# print(sugang)
+sugang['datastructure'] = 'park'    #update
+print(sugang)
+print(sugang["db"])
+print(sugang.get('db'))
+print(sugang.get('opensource'))
+print(sugang.get('opensource', 'not exist'))
+print(sugang.items())
+print(sugang.keys())
+print(sugang.values())
