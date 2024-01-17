92dc6ea2a7827446c3742f694d4ddff62b07abce
fkingsh <12200403@inha.edu>
2024-01-17 10:35:42 +0900

    v4.1

--- day04.py
+++ day04.py
@@ -1,21 +1,24 @@
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
-if n1 > n2:
-    n1, n2 = n2, n1
-
-for j in range(n1, n2+1):
-    decision2 = True
-    if j < 2:
-        pass
-    else:
-        i = 2
-        while i*i < n1:     #performance issue
-            if n1 % i == 0:
-                decision2 = False
-                break
-            i = i + 1
-        #     print(i, end=' ')     #loop count
-        if decision2:
-            pass
-            print(j, end=' ')
+t1 = (5)
+t2 = 5,
+t3 = (5,)
+t4 = (5, 7)
+t5 = 5, 7
+print(type(t1), type(t2), type(t3), type(t4), type(t5))
+# , 기호를 포함하면 tuple 형태가 된다.
+t6 = "python", "kim"    #packing
+print(type(t6), t6[1])
+subject, prof = t6      #unpacking
+print(prof)
+print(subject)
+# a, b, c =t6 ValueError: not enough values to unpack (expected 3, got 2)
+t7 = ()         #빈 튜플
+t8 = tuple()    #빈 튜플
+print(type(t7), type(t8))
+t9 = 1, 2, 3
+t10 = 1, 2, 4
+print(t9 == t10, t9 < t10, t9 > t10)
+t11 = 4.43,
+t12 = 3.97, 4.1, 3.27
+print(id(t11))
+t11 = t11 +t12  # t11 += t12
+print(id(t11))      # 원래있던 자료가 바뀌는 것이 아니다. 주소가 바뀐것 뿐
---
+++ dya03.py
@@ -0,0 +1,5 @@
+number = int(input())
+for i in range(number):
+    decision = True
+    if i < 2:
+        print(f"{number} is NOT a prime number")
