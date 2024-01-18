74cda417e739392733d038e3a9662054fd0329e8
fkingsh <12200403@inha.edu>
2024-01-18 14:26:29 +0900

    v5.4 helping  prime time

--- day05.py
+++ day05.py
@@ -1,7 +1,3 @@
-a = ["7", "-11", "3"]
-print(sum(map(int, a)))
-
-# x = 0
-# for i in a:
-#     x += int(i)
-# print(x)
+n1, n2 = map(int, input("Input first second number : ").split())
+n1, n2 = min(n1, n2), max(n1, n2)
+# prime_number 구하는 프로그래밍에서 추가적으로 보완해야 할 부분
