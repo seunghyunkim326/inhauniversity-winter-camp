aab77d2610892653fdeade8cb1aa664135a31a8a
fkingsh <12200403@inha.edu>
2024-01-18 09:08:48 +0900

    7.6 assignment3

--- day04.py
+++ day04.py
@@ -1,4 +1,8 @@
-# e2f = {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
-e2f = dict(dog='chien', cat='chat', walrus='morse')
-print(e2f.get('walrus'))        # 8.6 2)
-print(e2f.keys())
+year_lists = [i for i in range(2001, 2006)]             # 7.6 1)
+problem_7_2 = year_lists[3]                             # 7.6 2)
+print(year_lists[-1])                                   # 7.6 3)
+things = ["mozzarella", 'cinderella', 'salmonella']     # 7.6 4)
+for i in range(3):
+    print(things[i].title())
+print((things))
+print(problem_7_2)
