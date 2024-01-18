f6561ebf2943b55fd2622644e849a0f85b74119d
fkingsh <12200403@inha.edu>
2024-01-18 09:19:30 +0900

    assignment3 8.6 10)

--- day04.py
+++ day04.py
@@ -1,8 +1,2 @@
-year_lists = [i for i in range(2001, 2006)]             # 7.6 1)
-problem_7_2 = year_lists[3]                             # 7.6 2)
-print(year_lists[-1])                                   # 7.6 3)
-things = ["mozzarella", 'cinderella', 'salmonella']     # 7.6 4)
-for i in range(3):
-    print(things[i].title())
-print((things))
-print(problem_7_2)
+squares = {i : i*i for i in range(10)}
+print(squares)
