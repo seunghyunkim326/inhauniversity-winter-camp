17d555bfdd9242cbf936876da6baa0c3939a8d06
fkingsh <12200403@inha.edu>
2024-01-18 16:17:55 +0900

    prime detecting final

--- day05.py
+++ day05.py
@@ -1,12 +1,20 @@
-def my_range(first=0, last=5, step=1):
-    number = first
-    while number < last:
-        yield number
-        number += step
-r = my_range()
-print(r, type(r))
+# prime number
+def isprime(n):
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i**2 <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True
 
-for x in r:
-    print(x)
-for x in r:
-    print(x)
+n1, n2 = map(int, input("Input first second number : ").split())
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2+1):
+    if isprime(number):
+        print(number, end=' ')
