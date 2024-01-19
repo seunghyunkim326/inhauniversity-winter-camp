a48fa44f696debca2205c5ff4a1d5c538d5ae296
fkingsh <12200403@inha.edu>
2024-01-19 11:13:51 +0900

    v6.3

--- day05.py
+++ day05.py
@@ -28,20 +28,28 @@ import random
 # numbers = list()
 # for i in range(5):
 #     numbers.append(random.randint(1, 100))
+class OopsException(Exception):
+    pass
+
 numbers = [random.randint(1, 100) for i in range(10)]
 print(numbers)
 
 try:
     pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
     print(numbers[pick])
+    raise OopsException("Oops~~")        # exception!
 except IndexError as err:
     print(f"Out of range : Wrong index number\n{err}")
 except ValueError as err:
     print(f'Input Only Number~\n{err}')
 except ZeroDivisionError as err:
     print(f"The denominator cannot be 0.\n{err}")
+except OopsException as err:
+    print(f"Oops Oops {err}")
 except Exception as err:
     print(f"Error occurs : {err}")       # exception 먼저 사용 시 모든 에러를 잡아버림
+else:
+    print(f"Program terminate")
 
 # except Exception as err:
 #     print(err)                  # err은 system이 던져주는 에러 메세지
