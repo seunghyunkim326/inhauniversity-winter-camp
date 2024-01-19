aeeefb410d0828418a03ebf29dfa89b75a673b3b
fkingsh <12200403@inha.edu>
2024-01-19 10:31:23 +0900

    v6.1 v6.1

--- day05.py
+++ day05.py
@@ -1,23 +1,24 @@
-def test(f):
-    '''
-    데코레이터 함수, 함수 시작 시 start 출력, 함수 끝날 시 end 출력
-    :param f: function
-    :return: closure function
-    '''
-    def test_in(*args, **kwargs):       # *args : 무한한 변수 갯수     **kwargs : dictionary 형태로 받고 싶을 때
-        print('start')
-        r = f(*args, **kwargs)
-        print('end')
-        return r
-    return test_in
+def factorial_repetition(n) -> int:
+    """
+    반복문을 이용한 팩토리얼 함수
+    :param n: 정수
+    :return:
+    """
+    result = 1
+    for i in range(2, n+1):
+        result = result * i
+    return result
 
-#
-# def greeting():
-#     print("Hello")
-# t = test(greeting)
-# t()
-
-@test
-def greeting():
-    print("Hello")
-greeting()
+def factorial_recursion(n):
+    """
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수 int
+    :return:  function, int
+    """
+    if n == 1:
+        return  n
+    else:
+        return n * factorial_recursion(n-1)
+number = int(input("number : "))
+print(factorial_recursion(number))
+print(factorial_repetition(number))
