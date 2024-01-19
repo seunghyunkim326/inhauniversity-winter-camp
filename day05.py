78d02677e3e77624f5bcf47f7ce4e9c36c7f07da
fkingsh <12200403@inha.edu>
2024-01-19 09:17:20 +0900

    ver day5 assignment3 second porfessor

--- day05.py
+++ day05.py
@@ -3,20 +3,13 @@
 # 제너레이터 => 이터레이터 직접 만들 때 사용하는 구문
 # 함수내에 yield라는 키워드 포함 시 그 함수는 제너레이터가 됨
 # next()함수를 통해서 제너레이터 실행
-def get_odds():
-    for i in range(10):
-        if i % 2 == 1:
-            yield i
-A = get_odds()
+def get_odds(i) -> int:
+    for i in range(i, i+1, 2):
+        yield i
+count = 0
+A = get_odds(9)
 for x in A:
-    if x == 5:
-        print(x)
-
-# numbers = [1, 2, 3, 4, 5, 6]
-# # print("::".join(numbers))
-# print("::".join(map(str, numbers)))
-
-# numbers = list(range(1,10+1))
-#
-# print(list(filter(lambda x: x%2==1,numbers)))
-# print(list(filter(lambda y: y%2==0,numbers)))
+    count += 1
+    if count == 3:
+        print(f'Third number is {x}')
+        break
