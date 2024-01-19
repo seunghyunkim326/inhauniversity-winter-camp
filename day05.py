f488f2846de8238ba9ed0f6e7170ed8f8ca7716a
fkingsh <12200403@inha.edu>
2024-01-19 18:23:17 +0900

    v6.9

--- day05.py
+++ day05.py
@@ -15,7 +15,12 @@ class Pokemon:
     def __init__(self, name):
         self.name = name
         print(f"{name} 포켓몬스터 생성")
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
+charizard = Pokemon("리자몽")
 pickachu = Pokemon("피카츄")
 squirtle = Pokemon("꼬부기")
-print(pickachu)
-print(squirtle)
+
+charizard.attack(squirtle)
+print(pickachu.name)
+print(squirtle.name)
