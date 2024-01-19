e2eddf2843b6cdf2c76e88e0b24e0e84bfa76c42
fkingsh <12200403@inha.edu>
2024-01-19 18:16:08 +0900

    v6.8

--- day05.py
+++ day05.py
@@ -1,12 +1,21 @@
-class Pokemon():
-    def __init__(self):
-        pass
-pickachu = Pokemon()
-squirtle = Pokemon()
-pickachu.name = "피카츄"
-pickachu.nemesis = squirtle
-print(pickachu.name)
+# class Pokemon():
+#     def __init__(self):
+#         pass
+# pickachu = Pokemon()
+# squirtle = Pokemon()
+# pickachu.name = "피카츄"
+# pickachu.nemesis = squirtle
+# print(pickachu.name)
+# # print(pickachu.nemesis.name)
+# pickachu.nemesis.name = "꼬부기"
 # print(pickachu.nemesis.name)
-pickachu.nemesis.name = "꼬부기"
-print(pickachu.nemesis.name)
-print(squirtle.name)
+# print(squirtle.name)
+
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
+pickachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
+print(pickachu)
+print(squirtle)
