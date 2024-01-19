2c1bfb311b907db0dea0098af173ac84572619c5
fkingsh <12200403@inha.edu>
2024-01-19 13:46:37 +0900

    v6.5

--- day05.py
+++ day05.py
@@ -1,12 +1,34 @@
-def desc(f):
-    def wrapper():
-        print("study")
-        f()
-    return wrapper
-@desc
-def something():
-    print("do something~")
+# class Pokemon:
+#     def __init__(self, name):
+#         print(f"{name} 포켓몬 생성")
+#
+# pikachu = Pokemon("피카츄")
+# squirtle = Pokemon("꼬부기")
+# pikachu
+# squirtle
+# class Pokemon:
+#     pass
+#
+# pikachu = Pokemon()
+# squirtle = Pokemon()
+# pikachu.name = "피카츄"
+# pikachu.nemesis = squirtle
+# print(pikachu.name)
+# # print(pikachu.nemesis.name)
+# squirtle.name = "꼬부기"           # pikachu.nemesis.name = "꼬부기"   어차피 같음
+# print(pikachu.nemesis.name)
+class Pokemon :
+    def __init__(self, name):
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
 
-# s = desc(something)
-# s()
-something()
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
+
+
+charizard = Pokemon("리자몽")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
+charizard.attack(squirtle)
+# print(pikachu.name)
+# print(squirtle.name)
