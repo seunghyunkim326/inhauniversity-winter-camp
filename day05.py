6b31f6d5e19155bd4e7f55284ad872fedbf81d2a
fkingsh <12200403@inha.edu>
2024-01-19 15:08:37 +0900

    v6.6

--- day05.py
+++ day05.py
@@ -1,34 +1,40 @@
 # class Pokemon:
 #     def __init__(self, name):
-#         print(f"{name} 포켓몬 생성")
-#
-# pikachu = Pokemon("피카츄")
-# squirtle = Pokemon("꼬부기")
-# pikachu
-# squirtle
-# class Pokemon:
+#         self.name = name
+#     def attack(self, target):
+#         print(f"{self.name}이(가) {target.name}을(를) 공격!")
+# # class Pikachu:
+# class Pikachu(Pokemon):     # is-a
+#     def attack(self, target):
+#         print(f"{self.name}이(가) {target.name}을(를) 일렉트릭 공격")
+#     def electric_info(self):
+#         print(("전기 계열의 공격을 합니다"))
+# class Agumon:
 #     pass
+# p1 = Pikachu("피카츄")
+# p2 = Agumon("아구몬")
 #
-# pikachu = Pokemon()
-# squirtle = Pokemon()
-# pikachu.name = "피카츄"
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# # print(pikachu.nemesis.name)
-# squirtle.name = "꼬부기"           # pikachu.nemesis.name = "꼬부기"   어차피 같음
-# print(pikachu.nemesis.name)
-class Pokemon :
-    def __init__(self, name):
-        self.name = name
-        print(f"{name} 포켓몬스터 생성")
+# print(p1.name)
+# print(issubclass(Pikachu, Pokemon))
+# print(issubclass(Agumon, Pokemon))
 
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) 공격!")
-
-
-charizard = Pokemon("리자몽")
-pikachu = Pokemon("피카츄")
-squirtle = Pokemon("꼬부기")
-charizard.attack(squirtle)
-# print(pikachu.name)
-# print(squirtle.name)
+class Animal:
+    def says(self):
+        return 'I speak!'
+class Horse(Animal):
+    # def says(self):
+    #     return 'Neigh'
+    pass
+class Donkey(Animal):
+    def says(self):
+        return  'Hee-haw!'
+class Mule(Horse):
+    pass
+class Hinny(Horse, Donkey):
+    pass
+m1 = Mule()
+h1 = Hinny()
+print(h1.says())
+print(m1.says())
+print(Hinny.__mro__)
+print(Mule.__mro__)
