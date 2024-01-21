1f101d65a9aa22ac3f9121c032ede9e4737d0be8
fkingsh <12200403@inha.edu>
2024-01-21 23:34:00 +0900

    assignment 10.15 3)

--- assignment.py
+++ assignment.py
@@ -1,7 +1,8 @@
-# 10.15 2)
-# Thing2 클래스 생성 이 클래스 letters 속성에 값 'abc' 할당 후 letters 출력
-class Thing2:
+# 10.15 3)
+# Thing3 클래스 생성 이 클래스 letters 속성에 값 'xyz' 할당 후 letters 출력, letters 출력을 위해 객체 생성해야 하나? => 필요함
+class Thing3:
     def __init__(self, letter):
         self.letters = letter
-A = Thing2('abc')
+A = Thing3('xyz')
+print(A)
 print(A.letters)
