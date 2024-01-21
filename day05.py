41b52cfe5086695aaa6838730969a6d369806a4b
fkingsh <12200403@inha.edu>
2024-01-21 16:29:26 +0900

    v6.11

--- day05.py
+++ day05.py
@@ -1,14 +1,25 @@
+class FlyingMixin:
+    def fly(self):
+        return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.name}이(가) 수영을 합니다."
 class Pokemon:
     def __init__(self, name):
-        self.name = name
-        print(f"{name} 포켓몬스터 생성")
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) 공격!")
-class Pikachu(Pokemon):
-    def __init__(self, name, type):
-        super().__init__(name)
-        self.type = type
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) {self.type} 공격!")
-    def electric_info(self):
-        print("전기 계열의 공격을 합니다")
+        self.name = self
+    def attack(self):
+        print("공격!!")
+class Charizard(Pokemon, FlyingMixin):
+    pass
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리자몽")
+c1.attack()
+# Charizard.attack()      TypeError: Pokemon.attack() missing 1 required positional argument: 'self'
+# Charizard.attack(g1)
+print(g1.name)
+g1.name = "잉어킹"
+print(g1.name)              #왜 위에서 g1 객체 생성 시 "갸라도스"라고 name변수를 지정해 준 것이 아닌가?
+# g1 = Gyarados("갸라도스")
+# TypeError: Pokemon.__init__() takes 1 positional argument but 2 were given     self라는 객체 뒤에 매개 변수(name)을 통해 변수를 받아들여야한다.
--- dya03.py
+++ dya03.py
@@ -1,22 +1,25 @@
 class FlyingMixin:
     def fly(self):
         return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
-
 class SwimmingMixin:
     def swim(self):
         return f"{self.name}이(가) 수영을 합니다."
-
 class Pokemon:
     def __init__(self, name):
-        self.name = name
-
+        self.name = self
+    def attack(self):
+        print("공격!!")
 class Charizard(Pokemon, FlyingMixin):
     pass
-
 class Gyarados(Pokemon, SwimmingMixin):
     pass
-
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
-print(c1.fly())
-print(g1.swim())
+c1.attack()
+# Charizard.attack()      TypeError: Pokemon.attack() missing 1 required positional argument: 'self'
+# Charizard.attack(g1)
+print(g1.name)
+g1.name = "잉어킹"
+print(g1.name)              #왜 위에서 g1 객체 생성 시 "갸라도스"라고 name변수를 지정해 준 것이 아닌가?
+# g1 = Gyarados("갸라도스")
+# TypeError: Pokemon.__init__() takes 1 positional argument but 2 were given     self라는 객체 뒤에 매개 변수(name)을 통해 변수를 받아들여야한다.
